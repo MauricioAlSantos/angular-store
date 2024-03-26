fc92000d7b366e5bf3fd1a015bd18df3290b2052
Maurício Santos <mauricioalsantos@gmail.com>
2024-03-26 16:24:22 -0300

    base e componentização da lista de links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+import { MenuBarItemComponent } from './components/menu-bar/menu-bar-item/menu-bar-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    CardLabelComponent,
+    CardPricingComponent,
+    MenuBarItemComponent
   ],
   imports: [
     BrowserModule,
